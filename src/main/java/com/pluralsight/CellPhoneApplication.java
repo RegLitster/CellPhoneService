b44d47929c1298cfaf234a5618b710507a3bab4e
RegLitster <reglitster00@gmail.com>
2025-09-30 15:51:31 -0400

    Initial Commit

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,21 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        CellPhone Phone = new CellPhone();
+
+        System.out.print("Cell Number serialNumber");
+        String phonenumber = input.nextLine();
+        Phone.setPhoneNumber(phonenumber);
+
+        System.out.print("Cell Model");
+        String model = input.nextLine();
+        Phone.setModel(model);
+
+        System.out.print();
 
     }
 }
